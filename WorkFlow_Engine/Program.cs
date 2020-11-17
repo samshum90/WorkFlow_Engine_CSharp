3416d98861a3da5aca928e72bd3107949ed6d4f9
Samuel Shum <55974596+samshum90@users.noreply.github.com>
2020-11-17 18:10:37 +0000

    Add project files.

---
+++ WorkFlow_Engine/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkFlow_Engine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkFlow workFlow = new WorkFlow();
+            workFlow.Add(new VideoUploader());
+            workFlow.Add(new CallWebService());
+            workFlow.Add(new SendEmail());
+            workFlow.Add(new ChangeStatus());
+
+            WorkFlowEngine engine = new WorkFlowEngine();
+            engine.Run( workFlow );
+
+            Console.ReadLine();
+        }
+    }
+
+    public interface ITask
+    {
+        void Execute();
+    }    
+    
+    public interface IWorkFlow
+    {
+        void Add(ITask task);
+        void Remove(ITask task);
+        IEnumerable<ITask> GetTasks();
+    }
+
+    public class WorkFlow : IWorkFlow
+    {
+
+        private readonly List<ITask> _tasks;
+
+        public WorkFlow()
+        {
+            _tasks = new List<ITask>();
+        }
+
+        public void Add(ITask task)
+        {
+            _tasks.Add(task);
+        }
+
+        public void Remove(ITask task)
+        {
+            _tasks.Remove(task);
+        }
+
+        public IEnumerable<ITask> GetTasks()
+        {
+            return _tasks;
+        }
+    }
+
+    class VideoUploader : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Uploading a video!");
+        }
+    }    
+    
+    class CallWebService : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Calling a Web Service...");
+        }
+    }
+    
+    class SendEmail : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Sending an Email...");
+        }
+    }
+    
+    class ChangeStatus : ITask
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Status Changed...");
+        }
+    }
+
+    public class WorkFlowEngine
+    {
+   
+        public void Run(IWorkFlow workFlow)
+        {
+            foreach (ITask T in workFlow.GetTasks())
+            {
+                T.Execute();
+            }
+        }
+    }
+}
